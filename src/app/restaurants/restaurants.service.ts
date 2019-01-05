f0f71ed112541ed5da5f264243dca04cdc6cf393
Erick Freitas <erick.bezerra-external@wafx.global>
2019-01-05 13:20:02 -0200

    Criando componentes de detalhes do restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { Http } from "@angular/http";
 import { Observable} from "rxjs/Observable"
 import { MEAT_API} from "../app.api"
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import { ErrorHandler } from '../app.error-handler'
 
 @Injectable()
 export class RestaurantsService{
@@ -12,5 +14,6 @@ export class RestaurantsService{
     retaurants(): Observable<Restaurant[]>{
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
 }
