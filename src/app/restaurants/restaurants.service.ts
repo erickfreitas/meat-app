03c4b1b5cb74da6efca2a321c709b5452ca3e1b1
Erick Freitas <erick.bezerra-external@wafx.global>
2019-01-06 09:40:03 -0200

    Itens do menu página de detalhes dos restaurantes.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { MEAT_API} from "../app.api"
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import { ErrorHandler } from '../app.error-handler'
+import { MenuItem } from "./restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService{
@@ -25,6 +26,12 @@ export class RestaurantsService{
 
     reviewsOfRestaurant(id:string) : Observable<any>{
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string) : Observable<MenuItem>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
