499bb5a1dffb8b22c90e2adc6b545d676e2214f3
Erick Freitas <erickfreitasbezerra@gmail.com>
2019-06-22 11:52:18 -0300

    implementando rota de login

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,20 +1,23 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var jsonServer = require("json-server");
 var fs = require("fs");
 var https = require("https");
+var auth_1 = require("./auth");
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
 // Add custom routes before JSON Server router
 server.get('/echo', function (req, res) {
     res.jsonp(req.query);
 });
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser);
+// Middleware para login
+server.post('/login', auth_1.handleAuthentication);
 server.use(function (req, res, next) {
     if (req.method === 'POST') {
         req.body.createdAt = Date.now();
@@ -31,3 +34,4 @@ var options = {
 https.createServer(options, server).listen(3001, function () {
     console.log('JSON Server is running on https://localhost:3001');
 });
+//# sourceMappingURL=server.js.map
